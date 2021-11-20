17fb6228c722e03d36f81c378002fe033796a641
Fabio Catino <fabivs9@gmail.com>
2021-11-20 12:53:02 +0100

    custom domain added

--- fabio/settings.py
+++ fabio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fabioc.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['fabioc.herokuapp.com', '127.0.0.1', 'portfolio.fabiocatino.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
